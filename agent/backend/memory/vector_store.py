adbfbd536928db7e410d632d7af22a097fa5a553
Taha_M <taha10440023@gmail.com>
2026-04-30 01:34:09 +0530

    google model 001 for embeddings

--- agent/backend/memory/vector_store.py
+++ agent/backend/memory/vector_store.py
@@ -30,7 +30,7 @@ logger = logging.getLogger(__name__)
 #     return vec.tolist()
 
 _embeddings = GoogleGenerativeAIEmbeddings(
-    model="models/text-embedding-004",
+    model="models/gemini-embedding-001",
     google_api_key=os.environ["GEMINI_API_KEY"],
     task_type="SEMANTIC_SIMILARITY",
 )
